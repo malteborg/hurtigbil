1a3d2f39d2bd7fca0b7fa78f6b9b62eecaa0d586
Malte Borg-Andersen <malteborg.andersen03@gmail.com>
2025-03-04 14:04:10 +0100

    Nye kommentarer!

--- test.cpp
+++ test.cpp
@@ -4,6 +4,16 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
 
     // This is a comment
+    // This is another comment
+    // This is a third comment
+    // This is a fourth comment
+    // This is a fifth comment
+    // This is a sixth comment
+    // This is a seventh comment
+    // This is an eighth comment
+    // This is a ninth comment
+    // This is a tenth comment
+    // Now im done.
     
     return 0;
 }
