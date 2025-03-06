4adad01f71e13295ce4731ac30c4cb10fe9c001e
Martin <martr24@student.sdu.dk>
2025-03-06 10:06:56 +0100

    God joke

--- test3.c
+++ test3.c
@@ -2,5 +2,6 @@
 
 int main() {
     printf("Why don't programmers like nature? It has too many bugs!\n");
+    printf("Haha, get it? Bugs? Like insects? But also like errors in code?\n");
     return 0;
 }
